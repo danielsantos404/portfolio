42315c6f11aaf01e4400cb8bfac787775391c49d
Daniel Santos <dan.danielfs@gmail.com>
2025-07-07 16:32:08 -0300

    Card structuring and styling

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,29 @@
+import Icon from "../icon/Icon";
 import "./Card.css";
+import cesarIcon from "../../assets/cesarIcon.png";
 
 function Card() {
-    return (
-        <div className="card">
-        <h2 className="card-title">Card Title</h2>
-        <p className="card-content">This is some content inside the card.</p>
+  return (
+    <div className="card">
+      <h4 className="card-title">Projeto Teste</h4>
+      <div className="card-content">
+        <div className="card-img-container"></div>
+        <div className="card-icons-container">
+          <Icon src={cesarIcon} />
+          <Icon src={cesarIcon} />
+          <Icon src={cesarIcon} />
+          <Icon src={cesarIcon} />
+          <Icon src={cesarIcon} />
+          <Icon src={cesarIcon} />
+          <Icon src={cesarIcon} />
+          <Icon src={cesarIcon} />
         </div>
-    );
+        <div className="card-btt-container">
+          <button className="card-button">VER MAIS</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
