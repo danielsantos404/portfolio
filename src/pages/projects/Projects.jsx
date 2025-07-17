83e948493490be23e9cab66d9b7dfb6001a74167
Daniel Santos <dan.danielfs@gmail.com>
2025-07-17 18:25:59 -0300

    Initial structure of the Projects page

---
+++ src/pages/projects/Projects.jsx
@@ -0,0 +1,15 @@
+import Header from "../../components/header/Header";
+import "./Projects.css";
+
+function Projects() {
+  return (
+    <>
+      <Header navItems={[{ label: "VOLTAR", href: "#about" }]} />
+      <main className="all-projects">
+
+      </main>
+    </>
+  );
+}
+
+export default Projects;
