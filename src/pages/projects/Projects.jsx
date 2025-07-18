c9c467ab9ea9a84b772939b60e61dacae0c636db
Daniel Santos <dan.danielfs@gmail.com>
2025-07-18 14:56:17 -0300

    Projects Page structure

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,12 +1,27 @@
 import "./Projects.css";
 import Header from "../../components/header/Header";
+import Card from "../../components/card/Card";
 
 function Projects() {
   return (
     <>
       <Header navItems={[{ label: "VOLTAR", href: "/" }]} />
       <main className="all-projects">
-
+        <h1>TODOS OS PROJETOS</h1>
+        <section className="projects-container">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </section>
       </main>
     </>
   );
