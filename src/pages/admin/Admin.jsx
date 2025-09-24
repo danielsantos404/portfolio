a09c7935b36d46522ac196b75d3a0f290a86fc0b
Daniel Santos <dan.danielfs@gmail.com>
2025-09-23 21:17:25 -0300

    Admin login prototype

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,15 +1,34 @@
 import "./Admin.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "../../components/button/Button";
 import ProjectForm from "./components/project-form/ProjectForm";
 import TechnologyForm from "./components/technology-form/TechnologyForm";
+import githubIcon from  "../../assets/githubIcon.svg";
 
 function Admin() {
   const [isProjectModalOpen, setProjectModalOpen] = useState(false);
   const [isTechnologyModalOpen, setTechnologyModalOpen] = useState(false);
+  const [showWelcomeModal, setShowWelcomeModal] = useState(true);
+
+  useEffect(() => {
+    setShowWelcomeModal(true);
+  }, []);
 
   return (
     <>
+      {showWelcomeModal && (
+        <div className="admin-modal-overlay">
+          <div className="admin-modal">
+            <h2>Autentique-se 🔐</h2>
+            <Button
+              imgUrl={githubIcon}
+              text="GitHub"
+              onClick={() => setShowWelcomeModal(false)}
+              style={{ margin: "24px auto", display: "block" }}
+            />
+          </div>
+        </div>
+      )}
       <main className="admin">
         <section className="project-list">
           <h1>PROJETOS</h1>
