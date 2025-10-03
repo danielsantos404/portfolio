1916829a2020489007a2fe5f9e56c5c66195aac6
Daniel Santos <dan.danielfs@gmail.com>
2025-10-02 23:17:36 -0300

    Conditional inversion of Icon colors

--- src/components/icon/Icon.jsx
+++ src/components/icon/Icon.jsx
@@ -1,14 +1,33 @@
 import "./Icon.css";
 
-function Icon({ src, alt = "ícone", bgColor = "var(--white)", size = "small" }) {
+function Icon({ src, bgColor, iconColor, title, size = "small" }) {
+  const containerStyle = {
+    width: size,
+    height: size,
+    backgroundColor: bgColor,
+  };
+
+  const iconStyle = {
+    backgroundColor: iconColor,
+    maskImage: `url(${src})`,
+    WebkitMaskImage: `url(${src})`,
+    maskSize: "contain",
+    WebkitMaskSize: "contain",
+    maskRepeat: "no-repeat",
+    WebkitMaskRepeat: "no-repeat",
+    maskPosition: "center",
+    WebkitMaskPosition: "center",
+  };
+
   const sizeClass = size === "large" ? "icon-large" : "icon-small";
+
   return (
     <div
+      style={containerStyle}
+      title={title}
       className={`icon-container ${sizeClass}`}
-      title="Teste"
-      style={{ background: bgColor }}
     >
-      <img src={src} alt={alt} className="icon-img" />
+      <div style={iconStyle} className="icon-vector" />
     </div>
   );
 }
