40009b797ddd6e41079c15284d688e13dcde7df6
Daniel Santos <dan.danielfs@gmail.com>
2025-10-03 22:14:57 -0300

    Functional Projects Page

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,11 +1,35 @@
 import "./Projects.css";
-import { useEffect } from "react";
 import Header from "../../components/header/Header";
 import Card from "../../components/card/Card";
+import { useEffect, useState  } from "react";
+import { db } from "../../firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "projects"));
+        const projectsData = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setProjects(projectsData);
+      } catch (error) {
+        console.error("Erro ao buscar projetos:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProjects();
   }, []);
 
   return (
@@ -14,18 +38,13 @@ function Projects() {
       <main className="all-projects">
         <h1>TODOS OS PROJETOS</h1>
         <section className="projects-container">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {loading ? (
+            <p className="loading-message">Carregando projetos...</p>
+          ) : (
+            projects.map((project) => (
+              <Card key={project.id} project={project} />
+            ))
+          )}
         </section>
       </main>
     </>
